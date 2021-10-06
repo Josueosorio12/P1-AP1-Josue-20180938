8f52fc6c5b4036235c03386eaf17cf94a0762db4
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-10-06 01:11:07 -0400

    Creando la Entidad

---
+++ Entidades/AportesEntidades.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_ap1_josueosorio_20180938.Entidades
+{
+    public class AportesEntidades
+    {
+        [Key]
+        public int AporteID { get; set; }
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+
+        public int Monto { get; set; }
+        public int Conteo { get; set; }
+
+        public int Total { get; set; }
+    }
+}
