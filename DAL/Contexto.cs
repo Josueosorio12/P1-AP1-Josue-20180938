5da7cb164fd92e167c50f1e84ab08b6783f9d8f5
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-10-06 01:18:34 -0400

    Creando la migracion

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using P1_ap1_josueosorio_20180938.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace P1_ap1_josueosorio_20180938.DAL
 {
-    class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<AportesEntidades> AportesEntidades { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"DATA Source=DATA\Registroaportes.db");
+        }
     }
 }
---
+++ Migrations/20211006051723_Migracion_inicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P1_ap1_josueosorio_20180938.Migrations
+{
+    public partial class Migracion_inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AportesEntidades",
+                columns: table => new
+                {
+                    AporteID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FechaIngreso = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Persona = table.Column<string>(type: "TEXT", nullable: true),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    Monto = table.Column<int>(type: "INTEGER", nullable: false),
+                    Conteo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Total = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AportesEntidades", x => x.AporteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AportesEntidades");
+        }
+    }
+}
